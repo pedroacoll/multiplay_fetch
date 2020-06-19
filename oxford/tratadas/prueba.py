6ac961d55eb0413c9c5114acb72aba95b8fd03e9
mesadeordenador <pedro.acoll@gmail.com>
2020-06-19 16:39:38 +0200

    mustafa ilhan data added

--- oxford/tratadas/prueba.py
+++ oxford/tratadas/prueba.py
@@ -1,36 +1,14 @@
-import csv
-import math
+import numpy as np
+from operator import itemgetter, attrgetter
+s=['X','D','C']
 
-filename= 'stringency_country_date.csv'
-estructura_paises=[]
-paises=[]
-
-fechas=[]
-estructura_fechas=[]
-with open(filename, 'r') as f:   
-    reader = csv.reader(f)     
-    for indice, row in enumerate(reader):  
-    	if indice == 0 :
-    		for i in row:
-    			estructura_paises.append('')
-    			paises.append(i)
-    	fechas.append(row[0])
-    	estructura_fechas.append('')
-
-#print(len(estructura_paises),paises,len(estructura_fechas))
-    fechas=fechas[1:]
-    #print(fechas)
-    f.close()
-
-for n in range(len(paises)):
-	datos=[]
-	datos2=''
-	with open(filename, 'r') as fg:
-		reader = csv.reader(fg)
-		print('la n es ' + str(n))
-		for row in reader:
-			datos.append(row[n])
-			datos2+=';'+row[n]
-		print(datos2)
-		#print(datos)
-	fg.close()
+print(s)
+n=len(s)
+L = map(lambda x:[str(x), s[x]], range(0,n))
+L=sorted(L,key=itemgetter(1))
+Q = np.array(L).flatten()
+print(Q)
+print(n)
+data = [('red', 1), ('blue', 1), ('red', 2), ('blue', 2)]
+sorted(data, key=itemgetter(1))
+print(data)
---
+++ twitter/fetchdata.py
@@ -0,0 +1,11 @@
+import os
+import tweepy as tw
+import pandas as pd
+
+consumer_key= 'yourkeyhere'
+consumer_secret= 'yourkeyhere'
+access_token= 'yourkeyhere'
+access_token_secret= 'yourkeyhere'
+
+
+print('hola')
